d54caa074dda663f96d811169ffad5af5267c7c5
Saurabh Modh <saurabh.modh@hashedin.com>
2017-05-28 02:15:13 +0530

    Authentication & Permissions

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import url
+from django.conf.urls import include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    url(r'^userlist/$', views.UserList.as_view()),
+    url(r'^userlist/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^admin/', include(admin.site.urls)),
+]
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
